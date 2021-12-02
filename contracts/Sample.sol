5f37c5ff9c865f6df7ac6ab8f2a97d0fa26608e0
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2021-12-02 17:28:01 +0530

    sample code

---
+++ contracts/Sample.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.0;
+
+contract SampleCode {
+    string public helloWorld = "Hello World";
+
+    function getString() public returns(string) {
+        return helloWorld;
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+import {HardhatUserConfig} from 'hardhat/types';
+const config = {
+  solidity: {
+    version: '0.7.6',
+  }
+};
+export default config;
