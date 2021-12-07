ac960084053a6ba598b177c6289dc4450efe2edb
Abhishek <77354191+AbhishekG-1plus@users.noreply.github.com>
2021-12-07 14:34:04 +0530

    Delete Mock_Token.sol

--- Mock_Token.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.4;
-
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-contract MockToken is ERC20 {
-  constructor() ERC20('Mock Token', 'MTK') {
-    _mint(msg.sender, 1 * 10 ** 18);
-  }
-}
